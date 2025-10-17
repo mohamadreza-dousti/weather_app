e5e2ddcbc853dfbf61ab65e2709d97049ec1c798
mohamadreza <mohamadrezadoustii84@gmial.com>
2025-10-17 12:59:11 +0330

    Create main script to initialize and run weather app

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from base import weather_app_data
+
+class OpenWeather(weather_app_data):
+    __url = "https://api.openweathermap.org/data/2.5/weather"
+
+    def __init__(self, city_name, key="e86cd45bd2554daabe3dbe23a2c03cb5"):
+        self.city_name = city_name
+        self.__key = key
+        self.__params = {"q" : self.city_name, "appid" : self.__key}
+
+    def get_weather(self):
+        form = requests.get(f"{OpenWeather.__url}", params=self.__params)
+        form_json = form.json()
+        result = {"temp" : form_json["main"]["temp"], "temp_min" : form_json["main"]["temp_min"],
+                  "temp_max" : form_json["main"]["temp_max"], "sea_level" : form_json["main"]["sea_level"],
+                  "city" : form_json["name"], "country" : form_json["sys"]["country"]}
+
+        return (f"country:{result["country"]}\ncity:{result["city"]}\ntemp:{result["temp"]}\n"
+                f"temp min:{result["temp_min"]}\ntemp max:{result["temp_max"]}\nsea level:{result["sea_level"]}")
+
+
+if __name__ == "__main__":
+    weather_obj = OpenWeather("Tehran")#city_name, api_key
+    result_tehran = weather_obj.get_weather()
+    print(result_tehran, "\n")
+    weather_obj = OpenWeather("Ardabil")#city_name, api_key
+    result_ardabil = weather_obj.get_weather()
+    print(result_ardabil, "\n")
+
+
+
+
+
+
